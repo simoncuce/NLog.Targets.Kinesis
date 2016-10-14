19144feec35e79b02469fa7c006510dffc101d85
simoncuce <simon@cuce.com.au>
2016-10-14 17:09:25 +1100

    Start Project

---
+++ NLog.Targets.Kinesis/KinesisTarget.cs
@@ -0,0 +1,36 @@
+﻿#region
+
+using NLog.Config;
+
+#endregion
+
+namespace NLog.Targets.Kinesis
+{
+    [Target("MyFirst")]
+    public sealed class KinesisTarget : TargetWithLayout
+    {
+        private readonly KinesisUpload _upload;
+
+        public KinesisTarget()
+        {
+            _upload = new KinesisUpload(AwsKey, AwsSecret, AwsRegion);
+        }
+
+        [RequiredParameter]
+        public string AwsKey { get; set; }
+
+        [RequiredParameter]
+        public string AwsSecret { get; set; }
+
+        [RequiredParameter]
+        public string AwsRegion { get; set; }
+
+        [RequiredParameter]
+        public string Stream { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            _upload.Write(Layout.Render(logEvent), Stream);
+        }
+    }
+}
---
+++ NLog.Targets.Kinesis/KinesisUpload.cs
@@ -0,0 +1,56 @@
+﻿#region
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Kinesis;
+using Amazon.Kinesis.Model;
+
+#endregion
+
+namespace NLog.Targets.Kinesis
+{
+    public class KinesisUpload
+    {
+        private readonly AmazonKinesisClient _client;
+        private readonly string _partitionKey;
+
+        public KinesisUpload(string awsKey, string awsSecret, string region, string partitionKey = "")
+        {
+            _partitionKey = string.IsNullOrEmpty(partitionKey) ? Guid.NewGuid().ToString() : partitionKey;
+            _client = new AmazonKinesisClient(awsKey, awsSecret, RegionEndpoint.GetBySystemName(region));
+        }
+
+        public PutRecordResponse Write(string logMessage, string streamName)
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(logMessage)))
+            {
+                var requestRecord = new PutRecordRequest
+                {
+                    StreamName = streamName,
+                    Data = ms,
+                    PartitionKey = _partitionKey
+                };
+
+                return _client.PutRecord(requestRecord);
+            }
+        }
+
+        public async Task<PutRecordResponse> WriteAsync(string logMessage, string streamName)
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(logMessage)))
+            {
+                var requestRecord = new PutRecordRequest
+                {
+                    StreamName = streamName,
+                    Data = ms,
+                    PartitionKey = _partitionKey
+                };
+
+                return await _client.PutRecordAsync(requestRecord);
+            }
+        }
+    }
+}
